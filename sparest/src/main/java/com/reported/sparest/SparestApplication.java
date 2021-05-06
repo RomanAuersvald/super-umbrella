a7db21fa8e9d1455285f2edb995f4dbf5fcaec33
Roman Auersvald <roman.auersvald@seznam.cz>
2021-05-06 14:51:01 +0200

    sparest fix, projects docker ready

--- sparest/src/main/java/com/reported/sparest/SparestApplication.java
+++ sparest/src/main/java/com/reported/sparest/SparestApplication.java
@@ -1,11 +1,8 @@
 package com.reported.sparest;
 
-import com.reported.sparest.controller.LoginController;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
